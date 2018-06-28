71aed3f7af50b81e956715c942c192fa2ba67490
Aronja <anovicky@thoughtworks.com>
2018-06-28 19:32:12 +0100

    [AN] add Product and PorductList Component

---
+++ public/js/app-1.js
@@ -0,0 +1,13 @@
+class ProductList extends React.Component {
+  render() {
+  return(
+    <div className='ui unstackable items'>Hello World!
+    </div>
+  );
+}
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
